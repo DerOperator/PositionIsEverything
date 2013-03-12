38d492d72d0398faecafb2848dcf408785b870d6
DerOperator <idonthaveanaddress@hotmail.com>
2013-03-12 23:41:07 +0100

    GoogleAnalytics

--- sidepages/auto-enableAddClassOnKey.js
+++ sidepages/auto-enableAddClassOnKey.js
@@ -11,7 +11,18 @@
 /*
 	the ondocload "event" is triggered by a script call before the closing body tag:
 		<script type="text/javascript">if(window.bv_docLoaded)bv_docLoaded();</script>
-		</body>
+		<script type="text/javascript">
+  var _gaq = _gaq || [];
+  _gaq.push(['_setAccount', 'UA-39235363-1']);
+  _gaq.push(['_trackPageview']);
+
+  (function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  })();
+</script>
+</body>
 
 	it is the event before onload that indicates the document is done downloading;
 	after the func declaration we set two handlers to make sure events assigned to
